5847e1e060f3f26ec98771021f9287e7e0539424
Petr Schefzu <petr@oakslab.com>
2024-07-26 14:14:09 +0200

    feat: Performance tests

--- tests/command-processor.test.ts
+++ tests/command-processor.test.ts
@@ -16,7 +16,7 @@ describe('Command processor should', () => {
     const processor = new CommandProcessor(cache);
     expect(await processor.read(request)).toBe('lemon');
 
-    cache.clear();
+    await cache.clear();
   });
 
   test('should cache given file and return requested line', async () => {
@@ -32,17 +32,39 @@ describe('Command processor should', () => {
     expect(cache.exists()).toBe(true);
     expect(cache.get(request.lineNumber)).toBe('lemon');
 
-    cache.clear();
+    await cache.clear();
   });
 
-  test.skip('should benefit from cached file while called multiple times', async () => {
-    const request: ReadSettings = {
+  test('should benefit from cached file while called multiple times', async () => {
+    const lastLine: ReadSettings = {
       fileName: 'input_file_3.txt',
-      lineNumber: 3,
+      lineNumber: 39998,
     };
 
-    const cache = new FileCacheManager(request.fileName);
+    const oneBeforeTheLastLine: ReadSettings = {
+      fileName: 'input_file_3.txt',
+      lineNumber: 39997,
+    };
+
+    const cache = new FileCacheManager(lastLine.fileName);
     const processor = new CommandProcessor(cache);
-    expect(await processor.read(request)).toBe('lemon');
-  });
+
+    const timer_0 = Date.now();
+    expect(await processor.read(lastLine)).toBe(
+      'The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
+    );
+    const timer_1 = Date.now();
+    expect(await processor.read(oneBeforeTheLastLine)).toBe(
+      'Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.',
+    );
+    const timer_2 = Date.now();
+
+    const firstRun = timer_1 - timer_0;
+    const secondRun = timer_2 - timer_1;
+
+    expect(firstRun).not.toBeCloseTo(0);
+    expect(secondRun).toBeCloseTo(0);
+
+    await cache.clear();
+  }, 10_000);
 });
