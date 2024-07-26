de3ab0d54e54ab3065e0fbd96042b266e20268b5
Petr Schefzu <petr@oakslab.com>
2024-07-26 13:37:28 +0200

    feat: Add prettier

--- src/command-processor.ts
+++ src/command-processor.ts
@@ -3,33 +3,35 @@ import path from 'path';
 import readline from 'readline';
 
 export interface ReadSettings {
-    fileName: string;
-    lineNumber: number;
+  fileName: string;
+  lineNumber: number;
 }
 
 export class CommandProcessor {
-    public async read(file: ReadSettings): Promise<string> {
-        const filePath = path.join(__dirname, 'files', file.fileName);
-        /**
-         * It is important to use streams when reading large files,
-         * because it allows you to read the file in chunks, instead of loading the entire file into memory.
-         **/
-        const input = fs.createReadStream(filePath);
-        
-        // Readline is native to Node.js and is used to read files line by line.
-        const rl = readline.createInterface({
-            input,
-            crlfDelay: Infinity
-        });
+  public async read(file: ReadSettings): Promise<string> {
+    const filePath = path.join(__dirname, 'files', file.fileName);
+    /**
+     * It is important to use streams when reading large files,
+     * because it allows you to read the file in chunks, instead of loading the entire file into memory.
+     **/
+    const input = fs.createReadStream(filePath);
 
-        let currentLine = 0;
-        for await (const line of rl) {
-            if (currentLine === file.lineNumber) {
-                return line;
-            }
-            currentLine++;
-        }
+    // Readline is native to Node.js and is used to read files line by line.
+    const rl = readline.createInterface({
+      input,
+      crlfDelay: Infinity,
+    });
 
-        throw new Error(`Line ${file.lineNumber} not found in file ${file.fileName}`);
+    let currentLine = 0;
+    for await (const line of rl) {
+      if (currentLine === file.lineNumber) {
+        return line;
+      }
+      currentLine++;
     }
+
+    throw new Error(
+      `Line ${file.lineNumber} not found in file ${file.fileName}`,
+    );
+  }
 }
--- tests/command-processor.test.ts
+++ tests/command-processor.test.ts
@@ -1,13 +1,13 @@
 import { CommandProcessor, ReadSettings } from '../src/command-processor';
 
 describe('Command processor should', () => {
-    test('read given file and return requested line', async() => {
-        const request: ReadSettings = {
-            fileName: 'input_file.txt',
-            lineNumber: 3
-        };
+  test('read given file and return requested line', async () => {
+    const request: ReadSettings = {
+      fileName: 'input_file.txt',
+      lineNumber: 3,
+    };
 
-        const processor = new CommandProcessor();
-        expect(await processor.read(request)).toBe('lemon');
-    });
+    const processor = new CommandProcessor();
+    expect(await processor.read(request)).toBe('lemon');
+  });
 });
